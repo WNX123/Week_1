834b72d1aee73ecb73fdb16504caa8b20652dc30
WNX123 <148414978+WNX123@users.noreply.github.com>
2023-12-14 15:43:47 -0800

    Create week_1.py

---
+++ week_1.py
@@ -0,0 +1,15 @@
+# Put down your name
+my_name = "Wendy"
+# Put down the course name you're working on
+course_name = "Your First Python Program"
+# Current month (in integer please!)
+curr_month = 9 # change this to ..?
+
+print(f"Hello {my_name}! The course ends on Dec, which is {12-curr_month} months from now.")
+print("Hope you have a great time learing this course")
+
+today = 86.7
+yesterday = 87.0
+# print out the average temperuatre of today and yesterday
+avg_temp = (today+yesterday)/2 # fill in here!
+print(f"average temperature is {avg_temp}")
